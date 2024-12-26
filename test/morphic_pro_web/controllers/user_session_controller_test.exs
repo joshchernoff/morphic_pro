add67fad82942909a8e890fce3146943cf395871
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2024-12-25 19:35:30 -0800

    fix: tests

--- test/morphic_pro_web/controllers/user_session_controller_test.exs
+++ test/morphic_pro_web/controllers/user_session_controller_test.exs
@@ -83,14 +83,14 @@ defmodule MorphicProWeb.UserSessionControllerTest do
 
   describe "DELETE /users/log_out" do
     test "logs the user out", %{conn: conn, user: user} do
-      conn = conn |> log_in_user(user) |> delete(~p"/users/log_out")
+      conn = conn |> log_in_user(user) |> get(~p"/users/log_out")
       assert redirected_to(conn) == ~p"/"
       refute get_session(conn, :user_token)
       assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Logged out successfully"
     end
 
     test "succeeds even if the user is not logged in", %{conn: conn} do
-      conn = delete(conn, ~p"/users/log_out")
+      conn = get(conn, ~p"/users/log_out")
       assert redirected_to(conn) == ~p"/"
       refute get_session(conn, :user_token)
       assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Logged out successfully"
